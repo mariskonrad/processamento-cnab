bfeb246dc90acf39a64ab8e89ab382f9a27cc6c5
Marisbel Konrad <marisbelk@gmail.com>
2026-01-14 21:46:34 -0300

    create step

--- backend/src/main/java/com/marisbelkonrad/backend/BatchConfig.java
+++ backend/src/main/java/com/marisbelkonrad/backend/BatchConfig.java
@@ -5,26 +5,42 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.parameters.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.infrastructure.item.ItemProcessor;
+import org.springframework.batch.infrastructure.item.ItemReader;
+import org.springframework.batch.infrastructure.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BatchConfig {
-    private PlatformTransactionManager platformTransactionManager;
+    private PlatformTransactionManager transactionManager;
+    private JobRepository jobRepository;
 
-    public BatchConfig(PlatformTransactionManager platformTransactionManager) {
-        this.platformTransactionManager = platformTransactionManager;
+    public BatchConfig(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
+        this.transactionManager = transactionManager;
+        this.jobRepository = jobRepository;
     }
 
     @Bean
-    Job job(Step step, JobRepository jobRepository) {
+    Job job(Step step) {
         // O funcionamento do batch é muito baseado em máquina de estados.
         // Metadados com informações como quais jobs e steps já foram executados ficam armazenados no JobRepository
         return new JobBuilder("job", jobRepository)
                 .start(step)
                 .incrementer(new RunIdIncrementer())
                 .build();
+    }
 
+    @Bean
+    Step step(ItemReader<TransacaoCNAB> reader, ItemProcessor<TransacaoCNAB, Transacao> processor, ItemWriter<Transacao> writer) {
+        return new StepBuilder("step", jobRepository)
+                .<TransacaoCNAB, Transacao>chunk(1000)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
+                .transactionManager(transactionManager)
+                .build();
     }
 }
---
+++ backend/src/main/java/com/marisbelkonrad/backend/Transacao.java
@@ -0,0 +1,17 @@
+package com.marisbelkonrad.backend;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+
+public record Transacao(
+        Long id,
+        Integer tipo,
+        Date data,
+        BigDecimal valor,
+        Long cpf,
+        String cartao,
+        Time hora,
+        String donoDaLoja,
+        String nomeDaLoja) {
+}
